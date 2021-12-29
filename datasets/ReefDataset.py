d2a250b6da0288d3fb2f80ea3d0fad2f43b26968
Clement Apavou <apavou.clement@outlook.fr>
2021-12-29 14:58:35 +0100

    remove type

--- datasets/ReefDataset.py
+++ datasets/ReefDataset.py
@@ -90,8 +90,8 @@ class ReefDataset(Dataset):
             transformed = self.transforms(image=np.array(img),
                                           bboxes=target['boxes'],
                                           class_labels=target['labels'])
-            img = torch.as_tensor(transformed['image'], dtype=torch.float64)
-            target["boxes"] = torch.as_tensor(transformed['bboxes'], dtype=torch.float32)
+            img = torch.as_tensor(transformed['image'])
+            target["boxes"] = torch.as_tensor(transformed['bboxes'])
             target["labels"] = torch.as_tensor(transformed['class_labels'])
         
         print(img)
